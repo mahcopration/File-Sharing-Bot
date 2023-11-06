f1b59bc95d3f35360af6cdbdf68ac8d428168add
mahcopration <129929195+mahcopration@users.noreply.github.com>
2023-11-06 12:16:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/7thofficial/File-Sharing-Bot'>Click here</a>\n○ Channel : @ultroidofficial\n○ Support Group : @ultroid_Support</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Call_me_futurepilot'>Click here</a>\n○ Channel : @Call_me_futurepilot\n○ Support Group : @Call_me_futurepilot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
